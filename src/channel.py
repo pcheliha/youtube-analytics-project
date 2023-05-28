ceceb7c33e5c24de723fe10c876be1ef5fda8e19
Pchel <golovkov_1994@mail.ru>
2023-05-28 21:52:54 +0300

    написаны атрибуты в конструктор Chanel.   добавлены методы в класс

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,7 @@ from googleapiclient.discovery import build
 import json
 
 api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
+youtube = build('youtube', 'v3', developerKey=api_key
 
 
 class Channel:
